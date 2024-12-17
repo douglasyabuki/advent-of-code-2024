ee2e9d8e93ac90bf0e4e40cc0ad6692ffad5d767
douglasyabuki <douglasyabuki@gmail.com>
2024-12-17 06:03:29 -0300

    Add: Day 05

--- src/components/challenges/challenges-lists/2024/index.ts
+++ src/components/challenges/challenges-lists/2024/index.ts
@@ -2,5 +2,6 @@ import { day as day01 } from "./day01";
 import { day as day02 } from "./day02";
 import { day as day03 } from "./day03";
 import { day as day04 } from "./day04";
+import { day as day05 } from "./day05";
 
-export const challenges2024 = [day01, day02, day03, day04];
+export const challenges2024 = [day01, day02, day03, day04, day05];
